a510392b44e7f41b24d2f9feb14c8529b8757721
gouoy <ga0cord@gmail.com>
2024-11-22 15:41:17 +0900

    🔄

--- day13/src/App.jsx
+++ day13/src/App.jsx
@@ -1,14 +1,13 @@
 import Count from "./components/Count"
-
+import './App.css'
 
 function App() {
-  
 
   return (
-    <>
-     <h2>환율 계산기(KRW - EUR)</h2>
+    <div className="App">
+     <h2 className="title">환율 계산기(KRW - EUR)</h2>
      <Count/>
-    </>
+    </div>
   )
 }
 
